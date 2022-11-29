c3a5ba3239a5b3acc216185f8971488979658f25
PaulaPerazzo <mpperazzosampaio@gmail.com>
2022-11-29 09:38:01 -0300

    feature/adviceApi: advice api implemented

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { HomeContainer } from './styles';
 import { LogoCITi } from '../../assets';
-
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export const Home: React.FC = () => {
+
+    const [ advice, setAdvice ] = useState('')
+    
+    useEffect(() => {
+        axios.get('https://api.adviceslip.com/advice')
+            .then(res => {setAdvice(res.data.slip.advice)})
+            .catch(err => {console.log(err)})
+    }, [])
+
     return (
         <HomeContainer>
             <img src={LogoCITi} alt="" />
             <h1>ReactJS Boilerplate</h1>
+            <p> {advice} </p>
             <p>
                 Made with
                 {' '}
